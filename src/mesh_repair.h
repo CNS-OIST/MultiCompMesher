86efaef2f9079c5f4f65c4c7ba365c5aff414817
Tristan Carel <tristan.carel@gmail.com>
2020-05-11 02:37:21 +0200

    BBP review (#1)
    Add changes from BBP review for
    
    * Better formating
    * Replacing old pointers to smarter pointers
    * CMake file update

--- src/mesh_repair.h
+++ src/mesh_repair.h
@@ -23,54 +23,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Polyhedron_items_with_id_3.h>
 
-#include <CGAL/Polygon_mesh_processing/repair_polygon_soup.h>
 #include <CGAL/Polygon_mesh_processing/orient_polygon_soup.h>
-#include <CGAL/Polygon_mesh_processing/polygon_soup_to_polygon_mesh.h>
 #include <CGAL/Polygon_mesh_processing/orientation.h>
+#include <CGAL/Polygon_mesh_processing/polygon_soup_to_polygon_mesh.h>
+#include <CGAL/Polygon_mesh_processing/repair_polygon_soup.h>
 
 #include <CGAL/IO/OFF_reader.h>
 
-#include <vector>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "utility.h"
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel          K;
-typedef CGAL::Polyhedron_3<K, CGAL::Polyhedron_items_with_id_3>      PolyhedronWithID;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Polyhedron_3<K, CGAL::Polyhedron_items_with_id_3> PolyhedronWithID;
 
 int repair(std::string& filename) {
-  std::ifstream input(filename);
+    std::ifstream input(filename);
 
-  std::vector<K::Point_3> points;
-  std::vector<std::vector<std::size_t> > polygons;
+    std::vector<K::Point_3> points;
+    std::vector<std::vector<std::size_t>> polygons;
 
-  if(!input || !CGAL::read_OFF(input, points, polygons) || points.empty())
-  {
-    std::cerr << "Cannot open file " << filename << std::endl;
-    return EXIT_FAILURE;
-  }
-  CGAL::Polygon_mesh_processing::repair_polygon_soup(points, polygons);
-  CGAL::Polygon_mesh_processing::orient_polygon_soup(points, polygons);
+    if (!input || !CGAL::read_OFF(input, points, polygons) || points.empty()) {
+        std::cerr << "Cannot open file " << filename << std::endl;
+        return EXIT_FAILURE;
+    }
+    CGAL::Polygon_mesh_processing::repair_polygon_soup(points, polygons);
+    CGAL::Polygon_mesh_processing::orient_polygon_soup(points, polygons);
 
-  PolyhedronWithID mesh;
-  CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh(points, polygons, mesh);
+    PolyhedronWithID mesh;
+    CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh(points, polygons, mesh);
 
-  // Number the faces because 'orient_to_bound_a_volume' needs a face <--> index map
-  int index = 0;
-  for(PolyhedronWithID::Face_iterator fb=mesh.facets_begin(), fe=mesh.facets_end(); fb!=fe; ++fb)
-    fb->id() = index++;
+    // Number the faces because 'orient_to_bound_a_volume' needs a face <--> index map
+    int index = 0;
+    for (PolyhedronWithID::Face_iterator fb = mesh.facets_begin(), fe = mesh.facets_end(); fb != fe;
+         ++fb)
+        fb->id() = index++;
 
-  if(CGAL::is_closed(mesh))
-    CGAL::Polygon_mesh_processing::orient_to_bound_a_volume(mesh);
+    if (CGAL::is_closed(mesh))
+        CGAL::Polygon_mesh_processing::orient_to_bound_a_volume(mesh);
 
-  string_replace(filename, ".off", "_repaired.off");
-  std::ofstream out(filename);
-  out.precision(17);
-  if(!(out << mesh)) {
-      std::cerr << "Unable to write back repaired mesh data to " << filename <<".\n";
-      return EXIT_FAILURE;
-  }
-  std::cout << "Repaired mesh data has been written to " << filename << ".\n";
-  return EXIT_SUCCESS;
+    string_replace(filename, ".off", "_repaired.off");
+    std::ofstream out(filename);
+    out.precision(17);
+    if (!(out << mesh)) {
+        std::cerr << "Unable to write back repaired mesh data to " << filename << ".\n";
+        return EXIT_FAILURE;
+    }
+    std::cout << "Repaired mesh data has been written to " << filename << ".\n";
+    return EXIT_SUCCESS;
 }
